a7360a27a2e3d3fe5ea3725654854b96af0b65c7
parth kelkar <kelkar47@gmail.com>
2020-11-12 19:03:25 +0000

    Update fnn_utils.py
    updated file provided for better graph plotting performance

--- cw1-2020-files/fnn_utils.py
+++ cw1-2020-files/fnn_utils.py
@@ -6,39 +6,45 @@ import numpy as np
 
 plt.close("all")
 
-fig, axarr = plt.subplots(3,figsize=(10, 15))
+fig, axarr = plt.subplots(3, figsize=(6, 8))
+plt.subplots_adjust(wspace=1, hspace=1)
 fig.show()
 
-nx,ny = 5, 5    
-fig_p, axarr_p = plt.subplots(ny,nx,figsize=(15,15))
+nx, ny = 5, 5
+fig_p, axarr_p = plt.subplots(ny, nx, figsize=(7.5, 7.5))
+plt.subplots_adjust(wspace=1, hspace=1)
 fig_p.show()
 
-def display_predictions(network,show_pct=False):
-    fashion = ["T-shirt","Trouser","Pullover","Dress", "Coat","Sandal","Shirt","Sneaker","Bag","Boot"]
+
+def display_predictions(network, show_pct=False):
+    fashion = ["T-shirt", "Trouser", "Pullover", "Dress",
+               "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Boot"]
     permutation = np.random.permutation(len(network.testX))
     for y in range(ny):
         for x in range(nx):
             i = permutation[x*ny+y]
             predicted = network.predict(network.testX[i])
-            correct   = np.argmax(network.testY[i])
+            correct = np.argmax(network.testY[i])
             if predicted == correct:
                 color = "Greys"
             else:
                 color = "Reds"
-            label=fashion[predicted]
+            label = fashion[predicted]
             if show_pct:
-                label += " (" + '{0:.2f}'.format(network.predict_pct(predicted)) + ")"
-            axarr_p[y,x].set_xlabel(label,fontsize=20)
-            axarr_p[y,x].imshow(network.testX[i].reshape(28,28),cmap=color)
-            axarr_p[y,x].tick_params(
+                label += " (" + \
+                    '{0:.2f}'.format(network.predict_pct(predicted)) + ")"
+            axarr_p[y, x].set_xlabel(label, fontsize=10)
+            axarr_p[y, x].imshow(network.testX[i].reshape(28, 28), cmap=color)
+            axarr_p[y, x].tick_params(
                 axis='x',          # changes apply to the x-axis
                 which='both',      # both major and minor ticks are affected
                 bottom='off',      # ticks along the bottom edge are off
                 top='off',         # ticks along the top edge are off
-                labelbottom='off') # labels along the bottom edge are off
-            axarr_p[y,x].axes.get_yaxis().set_visible(False)
-    fig_p.canvas.draw()    
-    fig_p.canvas.flush_events()    # Help refresh the figure. we can delete it later if necessary
+                labelbottom='off')  # labels along the bottom edge are off
+            axarr_p[y, x].axes.get_yaxis().set_visible(False)
+    fig_p.canvas.draw()
+    plt.pause(0.01)
+
 
 def plot_stats(a, loss, test_acc_log, train_acc_log):
 
@@ -46,23 +52,24 @@ def plot_stats(a, loss, test_acc_log, train_acc_log):
 
     # Plot accuracies
     axarr[0].clear()
-    axarr[0].set_title("Classification accuracy",fontsize=18)
+    axarr[0].set_title("Classification accuracy", fontsize=18)
     axarr[0].plot(test_acc_log)
     axarr[0].plot(train_acc_log)
-    axarr[0].legend(["Test","Training"])
+    axarr[0].legend(["Test", "Training"])
 
     # Plot activations
     axarr[1].clear()
-    axarr[1].set_title("Activations",fontsize=18)
+    axarr[1].set_title("Activations", fontsize=18)
     axarr[1].violinplot(a)
 
     # Plot logs
     axarr[2].clear()
-    axarr[2].set_title("Loss",fontsize=18)
+    axarr[2].set_title("Loss", fontsize=18)
     axarr[2].plot(loss)
 
     fig.canvas.draw()
-    fig.canvas.flush_events()   # Help refresh the figure. we can delete it later if necessary
+    plt.pause(0.01)
+
 
 def load_mnist(path, kind='train'):
     import os
@@ -87,9 +94,8 @@ def load_mnist(path, kind='train'):
 
     return images, np.eye(10)[labels]
 
-    
-def read_data():
-    mnisttrainimages, mnisttrainlabels = load_mnist('fashion',kind='train')
-    mnisttestimages, mnisttestlabels = load_mnist('fashion',kind='t10k')
-    return mnisttrainimages, mnisttrainlabels, mnisttestimages, mnisttestlabels
 
+def read_data():
+    mnisttrainimages, mnisttrainlabels = load_mnist('fashion', kind='train')
+    mnisttestimages, mnisttestlabels = load_mnist('fashion', kind='t10k')
+    return mnisttrainimages, mnisttrainlabels, mnisttestimages, mnisttestlabels
